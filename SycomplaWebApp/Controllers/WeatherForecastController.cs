0fb8af07184579104636d2cf6c78fd48170ba890
d7p4n4 <bercikezs199999999@gmail.com>
2020-04-02 16:48:38 +0200

    Add project files.

---
+++ SycomplaWebApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Modul.Final.Class;
+using Newtonsoft.Json;
+
+namespace SycomplaWebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DummyController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<DummyController> _logger;
+
+        public DummyController(ILogger<DummyController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get([FromQuery(Name = "dummy")] string dummy)
+        {
+            //return dummy;
+
+            
+            DummyResponse dummyResponse =
+                DummyTwice(new DummyRequest()
+                {
+                    Dummy = dummy
+                });
+
+            if (dummyResponse.Result.Success())
+            {
+                return ("Minden szuper, dummy string: " + dummy);
+            } 
+            else
+            {
+                return dummyResponse.Result.Message;
+            }
+            
+        }
+
+        public DummyResponse DummyTwice(DummyRequest request)
+        
+        
+        {
+            DummyResult dummyResult = new DummyResult();
+            DummyResponse dummyResponse = new DummyResponse(); 
+
+            string dummyTwice = request.Dummy + " " + request.Dummy;
+
+            if (request.Dummy != null)
+            {
+                DummySuccessResponse JSONObjSuccess = new DummySuccessResponse();
+                JSONObjSuccess.Dummy = request.Dummy;
+
+                dummyResult.SuccessResponse = JSONObjSuccess;
+
+            } 
+            else
+            {
+
+                DummyFailedResponse JSONObjFailed = new DummyFailedResponse();
+                JSONObjFailed.StatusCode = 666665;
+
+                dummyResult.FailedResponse = JSONObjFailed;
+                
+            }
+
+            dummyResponse.DummyResult = dummyResult;
+
+
+            if (dummyResponse.DummyResult.FailedResponse == null)
+            {
+                dummyResponse.Result = new Ac4yProcessResult() { Code = Ac4yProcessResult.SUCCESS };
+            }
+            else
+            {
+                dummyResponse.Result = new Ac4yProcessResult() { Code = Ac4yProcessResult.FAIL, Message = "Nincs paraméter!" };
+            }
+            return dummyResponse;
+        }
+    }
+}
---
+++ SycomplaWebApp/Program.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace SycomplaWebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseUrls("https://89.46.75.137:8003")
+                .UseKestrel()
+                .UseStartup<Startup>();
+    }/*
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }*/
+}
