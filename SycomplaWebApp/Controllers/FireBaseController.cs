eb1ea89a2737f11e8d3cb8440d1880e4ab8e27da
d7p4n4 <bercikezs199999999@gmail.com>
2020-04-03 15:59:43 +0200

    1.20200403.5

---
+++ SycomplaWebApp/Controllers/FireBaseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Modul.Final.Class;
+
+namespace SycomplaWebApp.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FireBaseController : ControllerBase
+    {
+        [HttpPost]
+        [Route("FireBaseNotificationSend")]
+        public Ac4yServiceResponse FireBaseNotificationSend(FireBaseRequest request)
+        {
+            return new FireBaseMethods().SendNotification(request);
+        }
+    }
+}
