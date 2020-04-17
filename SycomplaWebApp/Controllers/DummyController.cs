53aa8da2523fe05d6d9990713613ce60fd4abee8
d7p4n4 <bercikezs199999999@gmail.com>
2020-04-17 12:08:14 +0200

    1.20200417.3

--- SycomplaWebApp/Controllers/DummyController.cs
+++ SycomplaWebApp/Controllers/DummyController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CSGIGServer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Modul.Final.Class;
--- SycomplaWebApp/Controllers/GIGServerController.cs
+++ SycomplaWebApp/Controllers/GIGServerController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CSGIGServer;
+using CSGIGUserServer;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
